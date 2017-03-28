31d424c6e2230ca69091a131a221714ee3b0919a
Christopher Grabski <grabc59@gmail.com>
2017-03-28 16:06:14 -0600

    add production pg config

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/portfolio_dev'
   },
-  test: {
+  production: {
     client: 'pg',
-    connection: 'postgres://localhost/portfolio_test'
+    connection: 'postgres://vmelvajncdawfd:74ad71c575babb2e620076d8f8ecf98c10123f01510978b11680aa9fb32bb7c8@ec2-54-235-245-255.compute-1.amazonaws.com:5432/d70qtigse7l0tn'
   }
 };
